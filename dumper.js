cc3cef74601551a21d4284f4aa08e377b8f438e2
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <96749372+a0zhar@users.noreply.github.com>
2023-03-06 19:06:54 +0100

    Update and rename dumper(include_in_page).js to dumper.js

--- dumper.js
+++ dumper.js
@@ -1,13 +1,15 @@
 function printf(){}//for debugging
+
 const hostIP = "ENTER_IP_HERE";
 const hostPort="5000";
-const webkitSize = 27262976;//27mb
-const libKernelSize = 300000;//300kb
-const libcSize = 1200000;//1.2mb
+
+const webkitSize = 27262976; //27mb
+const libKernelSize = 300000; //300kb
+const libcSize = 1200000; //1.2mb
 
 function dumpModule(address, size, name) {
   printf(`[*] Starting to dump the ${name} module!`);
-
+  
   const webSock = new WebSocket(`ws://${hostIP}:${hostPort}`);
   webSock.addEventListener("open", () => {
     let chunkSize = size / 1000;
@@ -15,7 +17,11 @@ function dumpModule(address, size, name) {
     let bytesRead = 0;
     while (bytesLeft > 0) {
       let buffer = read_mem(address + bytesRead, chunkSize);
-      webSock.send(buffer);
+      /* Over at the server, we split this string into 2 parts 
+      ([filename].bin, [filedata]) and using this, we no 
+      longer need to manually change the filename inside the 
+      server.js file. */
+      webSock.send(`${name}.bin,${buffer});
       bytesLeft -= chunkSize;
       bytesRead += chunkSize;
       buffer = null;
